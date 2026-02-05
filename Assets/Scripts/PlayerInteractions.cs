961a9cf21176787cfc29c83d243ec53dd1e826f4
Zhe <alannayou.z@gmail.com>
2026-02-04 21:25:13 -0500

    Added pick up and drop in desktop version
    The player can now pick up and drop the ball... but still cant throw it

--- Assets/Scripts/PlayerInteractions.cs
+++ Assets/Scripts/PlayerInteractions.cs
@@ -7,154 +7,85 @@ public class PlayerInteractions : MonoBehaviour
     public float interactRange = 5f;
     public Camera playerCamera;
     public CrosshairUI crosshairUIScript;
+    public Transform holdPoint;
+    //public KeyCode pickupKey = KeyCode.E;
+    private GameObject heldObject;
+    private Rigidbody heldObjectRb;
 
-    public GameObject player;
-    public Transform holdPos;
-    //if you copy from below this point, you are legally required to like the video
-    public float throwForce = 500f; //force at which the object is thrown at
-    public float pickUpRange = 5f; //how far the player can pickup the object from
-    private float rotationSensitivity = 1f; //how fast/slow the object is rotated in relation to mouse movement
-    private GameObject heldObj; //object which we pick up
-    private Rigidbody heldObjRb; //rigidbody of object we pick up
-    private bool canDrop = true; //this is needed so we don't throw/drop object when rotating the object
-    private int LayerNumber; //layer indexs
-    // Update is called once per frame
-
-    void Start()
-    {
-        LayerNumber = LayerMask.NameToLayer("holdLayer");
-
-    }
     void Update()
     {
         Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
         RaycastHit hit;
-        
+
+        //bool isLookingAtInteractable = false;
+
         // if object in 5f, detect tag
         if (Physics.Raycast(ray, out hit, interactRange))
         {
             if (hit.collider.CompareTag("Interactable"))
             {
                 crosshairUIScript.SetInteract(true);
-                /*
 
                 if (Keyboard.current.eKey.wasPressedThisFrame)
                 {
-                    Button button = hit.collider.GetComponent<Button>();
-                    button.Press();
-
-                }
-                */
-
-                if (Keyboard.current.eKey.wasPressedThisFrame)
-                {
-                    if (heldObj == null) // if not holding anything
+                    if (heldObject == null)
                     {
-                        PickUpObject(hit.transform.gameObject);
+                        PickUpObject(hit.collider.gameObject);
                     }
                     else
                     {
-                        if (canDrop == true)
-                        {
-                            StopClipping(); //prevents object from clipping through walls
-                            DropObject();
-                        }
+                        // Unparent
+                        heldObject.transform.SetParent(null);
+
+                        // Re-enable physics
+                        heldObjectRb.isKinematic = false;
+                        heldObjectRb.useGravity = true;
+
+                        heldObject = null;
+                        heldObjectRb = null;
                     }
 
                 }
-                
+
+               
+
                 return;
             }
         }
         crosshairUIScript.SetInteract(false);
-        if (heldObj != null) // player holding object
+        if (Keyboard.current.eKey.wasPressedThisFrame)
         {
-            MoveObject(); //keep object position at holdPos
-            RotateObject();
-            if (Input.GetKeyDown(KeyCode.E) && canDrop == true) //Mous0 (leftclick) is used to throw, change this if you want another button to be used)
+            if (heldObject != null)
             {
-                StopClipping();
-                ThrowObject();
+                heldObject.transform.SetParent(null);
+
+                // Re-enable physics
+                heldObjectRb.isKinematic = false;
+                heldObjectRb.useGravity = true;
+
+                heldObject = null;
+                heldObjectRb = null;
             }
         }
 
 
 
-    }
-    void PickUpObject(GameObject pickUpObj)
-    {
-        if (pickUpObj.GetComponent<Rigidbody>()) //make sure the object has a RigidBody
-        {
-            heldObj = pickUpObj; //assign heldObj to the object that was hit by the raycast (no longer == null)
-            heldObjRb = pickUpObj.GetComponent<Rigidbody>(); //assign Rigidbody
-            heldObjRb.isKinematic = true;
-            heldObjRb.transform.parent = holdPos.transform; //parent object to holdposition
-            heldObj.layer = LayerNumber; //change the object layer to the holdLayer
-            //make sure object doesnt collide with player, it can cause weird bugs
-            Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), true);
-        }
-    }
-    void DropObject()
-    {
-        //re-enable collision with player
-        Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
-        heldObj.layer = 0; //object assigned back to default layer
-        heldObjRb.isKinematic = false;
-        heldObj.transform.parent = null; //unparent object
-        heldObj = null; //undefine game object
-    }
-    void MoveObject()
-    {
-        //keep object position the same as the holdPosition position
-        heldObj.transform.position = holdPos.transform.position;
-    }
-    void RotateObject()
-    {
-        if (Input.GetKey(KeyCode.R))//hold R key to rotate, change this to whatever key you want
-        {
-            canDrop = false; //make sure throwing can't occur during rotating
 
-            //disable player being able to look around
-            //mouseLookScript.verticalSensitivity = 0f;
-            //mouseLookScript.lateralSensitivity = 0f;
 
-            float XaxisRotation = Input.GetAxis("Mouse X") * rotationSensitivity;
-            float YaxisRotation = Input.GetAxis("Mouse Y") * rotationSensitivity;
-            //rotate the object depending on mouse X-Y Axis
-            heldObj.transform.Rotate(Vector3.down, XaxisRotation);
-            heldObj.transform.Rotate(Vector3.right, YaxisRotation);
-        }
-        else
-        {
-            //re-enable player being able to look around
-            //mouseLookScript.verticalSensitivity = originalvalue;
-            //mouseLookScript.lateralSensitivity = originalvalue;
-            canDrop = true;
-        }
     }
-    void ThrowObject()
+    void PickUpObject(GameObject obj)
     {
-        //same as drop function, but add force to object before undefining it
-        Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
-        heldObj.layer = 0;
-        heldObjRb.isKinematic = false;
-        heldObj.transform.parent = null;
-        heldObjRb.AddForce(transform.forward * throwForce);
-        heldObj = null;
+        heldObject = obj;
+        heldObjectRb = obj.GetComponent<Rigidbody>();
+
+        // Disable physics
+        heldObjectRb.isKinematic = true;
+        heldObjectRb.useGravity = false;
+
+        // Parent to hold point
+        heldObject.transform.SetParent(holdPoint);
+        heldObject.transform.localPosition = Vector3.zero;
+        heldObject.transform.localRotation = Quaternion.identity;
     }
-    void StopClipping() //function only called when dropping/throwing
-    {
-        var clipRange = Vector3.Distance(heldObj.transform.position, transform.position); //distance from holdPos to the camera
-        //have to use RaycastAll as object blocks raycast in center screen
-        //RaycastAll returns array of all colliders hit within the cliprange
-        RaycastHit[] hits;
-        hits = Physics.RaycastAll(transform.position, transform.TransformDirection(Vector3.forward), clipRange);
-        //if the array length is greater than 1, meaning it has hit more than just the object we are carrying
-        if (hits.Length > 1)
-        {
-            //change object position to camera position 
-            heldObj.transform.position = transform.position + new Vector3(0f, -0.5f, 0f); //offset slightly downward to stop object dropping above player 
-            //if your player is small, change the -0.5f to a smaller number (in magnitude) ie: -0.1f
-        }
-    }
+
 }
